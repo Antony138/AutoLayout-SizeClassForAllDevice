8d14ad571405dccb46acef5f617a7e72d4cd62ff
Antony.Wong <aeq2005@163.com>
2015-10-29 11:05:36 +0800

    import project

---
+++ Project/Shopping_ALSC_Test/SpecialsDetailViewController.swift
@@ -0,0 +1,45 @@
+//
+//  SpecialsDetailViewController.swift
+//  Shopping_ALSC_Test
+//
+//  Created by YuHeng_Antony on 10/28/15.
+//  Copyright © 2015 Homni Electron Inc. All rights reserved.
+//
+
+import UIKit
+
+class SpecialsDetailViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var brandLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var originalPriceLabel: UILabel!
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Project/Shopping_ALSC_Test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Shopping_ALSC_Test
+//
+//  Created by YuHeng_Antony on 10/28/15.
+//  Copyright © 2015 Homni Electron Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
